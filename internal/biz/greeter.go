bfb05e592fc6525f9ea6c852a91107a83a01017b
mouuii <49775493+mouuii@users.noreply.github.com>
2023-06-15 10:24:00 +0800

    change to agent api

--- internal/biz/greeter.go
+++ internal/biz/greeter.go
@@ -3,7 +3,7 @@ package biz
 import (
 	"context"
 
-	v1 "devops-agent/api/helloworld/v1"
+	v1 "devops-agent/api/agent/v1"
 
 	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
--- internal/server/http.go
+++ internal/server/http.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-	v1 "devops-agent/api/helloworld/v1"
+	v1 "devops-agent/api/agent/v1"
 	"devops-agent/internal/conf"
 	"devops-agent/internal/service"
 
@@ -11,7 +11,7 @@ import (
 )
 
 // NewHTTPServer new an HTTP server.
-func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.Logger) *http.Server {
+func NewHTTPServer(c *conf.Server, agent *service.AgentService, logger log.Logger) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
@@ -27,6 +27,6 @@ func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.L
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
 	srv := http.NewServer(opts...)
-	v1.RegisterGreeterHTTPServer(srv, greeter)
+	v1.RegisterAgentHTTPServer(srv, agent)
 	return srv
 }
