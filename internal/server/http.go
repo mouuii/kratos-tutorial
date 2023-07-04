bf8f2b3282a2442a93366fc34094eee378455ef3
mouuii <49775493+mouuii@users.noreply.github.com>
2023-07-04 09:56:44 +0800

    add err response

--- internal/server/http.go
+++ internal/server/http.go
@@ -4,13 +4,36 @@ import (
 	v1 "devops-agent/api/agent/v1"
 	"devops-agent/internal/conf"
 	"devops-agent/internal/service"
-	"github.com/go-kratos/kratos/v2/middleware/validate"
-	"github.com/go-kratos/swagger-api/openapiv2"
-
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/validate"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/go-kratos/swagger-api/openapiv2"
 )
+
+// SelfErrorEncoder encodes the error to the HTTP response.
+//func SelfErrorEncoder(w http.ResponseWriter, r *http.Request, err error) {
+//	se := errors.FromError(err)
+//	codec, _ := http.CodecForRequest(r, "Accept")
+//
+//	type SelfErrResp struct {
+//		Code int `json:"errCode"`
+//	}
+//	e := SelfErrResp{
+//		int(se.Code),
+//	}
+//
+//	body, _ := codec.Marshal(e)
+//
+//	w.Header().Set("Content-Type", ContentType(codec.Name()))
+//	w.WriteHeader(int(se.Code))
+//	_, _ = w.Write(body)
+//}
+//
+//// ContentType returns the content-type with base prefix.
+//func ContentType(subtype string) string {
+//	return strings.Join([]string{"application", subtype}, "/")
+//}
 
 // NewHTTPServer new an HTTP server.
 func NewHTTPServer(c *conf.Server, agent *service.AgentService, logger log.Logger) *http.Server {
@@ -19,6 +42,7 @@ func NewHTTPServer(c *conf.Server, agent *service.AgentService, logger log.Logge
 			recovery.Recovery(),
 			validate.Validator(),
 		),
+		//http.ErrorEncoder(SelfErrorEncoder),
 	}
 	if c.Http.Network != "" {
 		opts = append(opts, http.Network(c.Http.Network))
